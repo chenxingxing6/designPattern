400607c58ce29a128994dbf914c6cacaec24371f
lanxinghua <lanxinghua@2dfire.com>
2019-07-22 09:59:37 +0800

    hutool工具

---
+++ src/test/java/hutool/HutoolTest.java
@@ -0,0 +1,14 @@
+package hutool;
+
+import cn.hutool.core.lang.Console;
+
+/**
+ * User: lanxinghua
+ * Date: 2019/7/22 09:41
+ * Desc:
+ */
+public class HutoolTest {
+    public static void main(String[] args) {
+        Console.log("35235213451");
+    }
+}
